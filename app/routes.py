aab604c48e7d9819dc0f3919a420a3d76de8d208
Lily <aulily09@gmail.com>
2023-04-21 19:31:39 -0700

    Created route to get all details for all planets

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify
 
-class Planet():
-    def __init__(self, id, name, description,is_planet):
+
+class Planet:
+    def __init__(self, id, name, description, is_planet):
         self.id = id
         self.name = name
         self.description = description
@@ -9,15 +10,36 @@ class Planet():
 
 
 planets = [
-    Planet(1, "Mercury", "Even though Mercury is the closest planet to the sun, it has ice on its surface.", True),
-    Planet(2, "Venus", "Venus is the hottest planet in our solar system and doesn't have any moons.", True),
+    Planet(
+        1,
+        "Mercury",
+        "Even though Mercury is the closest planet to the sun, it has ice on its surface.",
+        True,
+    ),
+    Planet(
+        2,
+        "Venus",
+        "Venus is the hottest planet in our solar system and doesn't have any moons.",
+        True,
+    ),
     Planet(3, "Earth", "Earth is not flat.", True),
     Planet(4, "Mars", "Mars once had water on its surface.", True),
     Planet(5, "Jupiter", "Jupiter is a great comet catcher.", True),
     Planet(6, "Saturn", "No one knows how old Saturn’s rings are.", True),
-    Planet(7, "Uranus", "Uranus is the only planet in the solar system whose axis is tilted nearly 98 degrees.", True),
+    Planet(
+        7,
+        "Uranus",
+        "Uranus is the only planet in the solar system whose axis is tilted nearly 98 degrees.",
+        True,
+    ),
     Planet(8, "Neptune", "Neptune has supersonic winds.", True),
-    Planet(9, "Pluto", "Pluto is a Dwarf Planet.", False)
+    Planet(9, "Pluto", "Pluto is a Dwarf Planet.", False),
 ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+@planets_bp.route("", methods=["GET"])
+def get_planets():
+    planet_dict = [vars(planet) for planet in planets]
+    return jsonify(planet_dict), 200
