b4f72d60d019ccd51b912702ca254d42aa04fb22
Lily <aulily09@gmail.com>
2023-05-02 11:42:14 -0700

    Model added

--- app/__init__.py
+++ app/__init__.py
@@ -17,9 +17,10 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # from flask import Blueprint
     from .routes import planets_bp
 
     app.register_blueprint(planets_bp)
+
+    from app.Model.planet import Planet
 
     return app
