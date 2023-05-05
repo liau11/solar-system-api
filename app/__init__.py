577c10bb7ecce178edaaadd78dc21e1bc3110621
mlhuynh <greenlinh@gmail.com>
2023-05-05 14:35:47 -0700

    Added if/else condition to determine which database to use

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,15 @@ migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    app.config[
-        "SQLALCHEMY_DATABASE_URI"
-    ] = "postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    if test_config is None:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
