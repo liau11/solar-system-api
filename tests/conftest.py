1e2c0f9461ee2f134f484db7304f05461409a765
mlhuynh <greenlinh@gmail.com>
2023-05-05 14:59:29 -0700

    Wrote app() fixure.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,24 @@
+import pytest
+from app import create_app
+from app import db
+from flask.signals import request_finisheded
+from app.models.planet import Planet
+
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+    
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
 
 
